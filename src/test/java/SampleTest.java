9e360af8c4abd001cc4ed6bf78aebc5bcdb74ab2
amjademter <amjad.emter.ae@gmail.com>
2026-01-17 16:23:14 +0200

    Add basic TestNG automation project with sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleAutomationTest() {
+        Assert.assertTrue(true);
+    }
+}
